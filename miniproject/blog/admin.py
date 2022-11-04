c588d3db467498c067f817046584f89e58309d5d
nio <niowiobio@gmail.com>
2022-11-04 13:28:58 +0000

    which columns will be displayed in the 'Articles' list

--- miniproject/blog/admin.py
+++ miniproject/blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Article
 
+# настройка представления в админке - укажем какие колонки будут выводиться в списке Articles:
+class ArticleAdmin(admin.ModelAdmin):
+    #выведем id и title и дату создания
+    list_display = ('id', 'title', 'created_at')
+
+
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
+
