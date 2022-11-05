b37a7cfdce3e9210eb499eb2ebf69271f09a2108
nio <niowiobio@gmail.com>
2022-11-05 16:25:16 +0000

    Filling in the base. List output via controller.

--- miniproject/blog/views.py
+++ miniproject/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 def home(request):
-    return HttpResponse('<h1>hello miniproject!</h1>')
+    posts = Article.objects.all()
+    res = '<h1>Список статей</h>'
+    for post in posts:
+        res += f'<div><h3>{post.title}</h3><div>{post.content}</div></div><hr>'
+    return HttpResponse(res)
 
 
 def test(request):
