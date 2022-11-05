880608c46d3d09436a7d02c9a354b44967af9964
nio <niowiobio@gmail.com>
2022-11-05 17:19:38 +0000

    get sorted articles

--- miniproject/blog/views.py
+++ miniproject/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Article
 
 def home(request):
-    posts = Article.objects.all()
+    posts = Article.objects.order_by('-created_at') # сортировка по полю даты создания в обратном порядке
     return render(request, 'blog/home.html', {'posts':posts})
 
 def test(request):
